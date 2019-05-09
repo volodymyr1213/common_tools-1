5ae20ea83e30b841baeabab689d1264e16ffd89c
Akmal Kadyrov <akmalkadyrov@Akmals-MacBook-Pro.local>
2019-05-08 22:47:44 -0600

    namespace added

---
+++ google_k8s_namespaces.tf
@@ -0,0 +1,17 @@
+ resource "kubernetes_namespace" "dev-namespace" {
+  metadata {
+    name = "dev-students" 
+  }
+}
+
+resource "kubernetes_namespace" "qa-namespace" {
+  metadata {
+    name = "qa-students" 
+  }
+}
+
+resource "kubernetes_namespace" "prod-namespace" {
+  metadata {
+    name = "prod-students" 
+  }
+} 
