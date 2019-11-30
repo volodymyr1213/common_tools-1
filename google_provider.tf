8361881c7f78b5df6547d1e07c9683e7dfb3f7e2
Farkhod <sadykovfarkhod@gmail.com>
2019-11-29 23:00:09 -0600

    Fixed project names to right value

--- google_provider.tf
+++ google_provider.tf
@@ -1,6 +1,6 @@
 # provider "google" {
 #   credentials   = "${file(var.project)}"   #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
-#   project       = "${var.project}"
+#   project       = "${var.google_project_id}"
 #   region        = "us-central1"
 # }
 
