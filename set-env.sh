0b9c33fded2107153e2f60331a2b68aa1e639e33
Anastasia Clipco <anastasiaclipco@gmail.com>
2020-02-26 20:08:30 -0600

    deployment

--- set-env.sh
+++ set-env.sh
@@ -1,6 +1,45 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-wget --quiet "https://raw.githubusercontent.com/fuchicorp/common_scripts/master/set-environments/terraform/google-set-env.sh" \
-  -O set-env >/dev/null
-source 'set-env' "$@"
-rm -rf "set-env"
+DIR=$(pwd)
+DATAFILE="$DIR/$1"
+#
+# FuchiCorp common script to set up Google terraform environment variables
+# these all variables should be created on your config file before you run script.
+# <ENVIRONMENT> <BUCKET> <DEPLOYMENT> <PROJECT> <CREDENTIALS>
+
+if [ ! -f "$DATAFILE" ]; then
+  echo "setenv: Configuration file not found: $DATAFILE"
+  return 1
+fi
+
+wget --quiet -O "$PWD/common_configuration.tfvars"\
+  "https://raw.githubusercontent.com/fuchicorp/main-fuchicorp/master/project-configuration/google_account_information.tfvars"
+
+
+BUCKET=$(sed -nr 's/^google_bucket_name\s*=\s*"([^"]*)".*$/\1/p'             "$PWD/common_configuration.tfvars")
+PROJECT=$(sed -nr 's/^google_project_id\s*=\s*"([^"]*)".*$/\1/p'             "$PWD/common_configuration.tfvars")
+ENVIRONMENT=$(sed -nr 's/^deployment_environment\s*=\s*"([^"]*)".*$/\1/p'    "$DATAFILE")
+DEPLOYMENT=$(sed -nr 's/^deployment_name\s*=\s*"([^"]*)".*$/\1/p'            "$DATAFILE")
+CREDENTIALS=$(sed -nr 's/^credentials\s*=\s*"([^"]*)".*$/\1/p'               "$DATAFILE")
+
+if [ -z "$ENVIRONMENT" ]
+then
+    echo "setenv: 'deployment_environment' variable not set in configuration file."
+    return 1
+fi
+
+if [ -z "$BUCKET" ]
+then
+  echo "setenv: 'google_bucket_name' variable not set in configuration file."
+  return 1
+fi
+
+if [ -z "$PROJECT" ]
+then
+    echo "setenv: 'google_project_id' variable not set in configuration file."
+    return 1
+fi
+
+if [ -z "$CREDENTIALS" ]
+then
+    echo "setenv: 'credentials' file not set in configuration file."
