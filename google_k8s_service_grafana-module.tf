398985daa3cbe2b77318291b808dcf8ef5708b46
Huseyin Akten <husakten@gmail.com>
2020-03-02 09:38:52 -0600

    yes

--- google_k8s_service_grafana-module.tf
+++ google_k8s_service_grafana-module.tf
@@ -1,5 +1,6 @@
 module "grafana_deploy" {
   source                 = "fuchicorp/chart/helm"
+  version                = "0.0.2"
   deployment_name        = "${var.grafana["grafana-name"]}"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "grafana.${var.google_domain_name}"
