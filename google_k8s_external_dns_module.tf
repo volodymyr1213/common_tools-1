1ca39970601e635230674811a3aace86de5cded3
Farkhod Sadykov <sadykovfarkhod@gmail.com>
2020-03-03 14:12:47 -0600

    Deleted some comments

--- google_k8s_external_dns_module.tf
+++ google_k8s_external_dns_module.tf
@@ -1,19 +1,3 @@
-/* module "helm_deploy" {
-  source = "fuchicorp/chart/helm"
-
-  deployment_name        = "${var.deployment_name}" 
-  deployment_environment = "${var.deployment_environment}" 
-  deployment_endpoint    = "external-dns.${var.google_domain_name}"
-  deployment_path        = "external-dns"
-
-
-  template_custom_vars = {
-    deployment_image = "nginx"    
-    google_project      = "${var.google_project}"
-
-  }
-}*/
-
 
 # Reading the  template and  setting terraform values
 module "helm_deploy" {
