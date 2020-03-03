c7242ab60538a749ac456dd8a36c18a936f52a44
csamatov96 <csamatov96@gmail.com>
2020-03-03 17:01:45 +0000

    edited module file

--- google_k8s_external_dns_module.tf
+++ google_k8s_external_dns_module.tf
@@ -1,4 +1,4 @@
-module "helm_deploy" {
+/* module "helm_deploy" {
   source = "fuchicorp/chart/helm"
 
   deployment_name        = "${var.deployment_name}" 
@@ -12,4 +12,31 @@ module "helm_deploy" {
     google_project      = "${var.google_project}"
 
   }
+}*/
+
+
+# Reading the  template and  setting terraform values
+module "helm_deploy" {
+  source  = "fuchicorp/chart/helm"
+  version = "0.0.2"
+  deployment_name         = "external-dns-deployment"
+  deployment_environment  = "${var.deployment_environment}"
+  deployment_endpoint     = "${var.google_domain_name}"
+  deployment_path         = "external-dns"
+  template_custom_vars    = {
+    google_project        = "${var.google_project_id}"
+  }
 }
+resource "kubernetes_secret" "external_dns_secret" {
+  metadata {
+    name      = "google-service-account"
+    namespace = "${var.deployment_environment}"
+  }
+  data = {
+    "credentials.json" = "${file("${path.module}/common-service-account.json")}"
+  }
+  type = "generic"
+}
+
+
+
