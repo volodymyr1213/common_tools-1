4e26ab304cbf140a787d0ecf58989c594b3755da
csamatov96 <csamatov96@gmail.com>
2020-03-02 20:09:41 -0600

    added

--- google_k8s_external_dns_module.tf
+++ google_k8s_external_dns_module.tf
@@ -1,13 +1,15 @@
 module "helm_deploy" {
   source = "fuchicorp/chart/helm"
 
-
-  deployment_name        = "artemis-deployment"
+  deployment_name        = "${var.deployment_name}" 
   deployment_environment = "${var.deployment_environment}" 
-  deployment_endpoint = "${var.google_domain_name}"
-  deployment_path        = "artemis"
+  deployment_endpoint    = "externel-dns.${var.google_domain_name}"
+  deployment_path        = "external-dns"
+
 
   template_custom_vars = {
-    deployment_image = "nginx"
+    deployment_image = "nginx"    
+    google_project_id      = "${var.google_project_id}"
+
   }
 }
