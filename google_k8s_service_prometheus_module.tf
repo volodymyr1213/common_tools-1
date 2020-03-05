243f21651ca9b1128ca30e7ef1c2afeada51d43d
Farkhod <sadykovfarkhod@gmail.com>
2020-03-04 19:06:37 -0600

    Fixed module file and ready to merge

--- google_k8s_service_prometheus_module.tf
+++ google_k8s_service_prometheus_module.tf
@@ -1,6 +1,6 @@
 module "prometheus_deploy" {
   source  = "fuchicorp/chart/helm"
-  deployment_name        = "${var.deployment_name}"
+  deployment_name        = "prometheus-deploy"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "prometheus.${var.google_domain_name}"
   deployment_path        = "prometheus"
