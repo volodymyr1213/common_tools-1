c06b95867233c90b13f2b7c3c2c8f4eb67570e7f
Huseyin Akten <husakten@gmail.com>
2020-03-09 18:53:36 -0500

    main-helm module is created

--- google_k8s_service_ingress.tf
+++ google_k8s_service_ingress.tf
@@ -1,44 +1,18 @@
 ## Before deploy ingress for each services it will make sure all services available
-resource "helm_release" "services_ingress" {
-  depends_on = [
-    "kubernetes_deployment.vault_deployment",
-    "kubernetes_service.vault_service",
-    "kubernetes_service.nexus_service",
-    "kubernetes_namespace.service_tools",
-    "kubernetes_service_account.tiller",
-    "kubernetes_secret.tiller",
-    "kubernetes_service.vault_service",
-    "kubernetes_cluster_role_binding.tiller_cluster_rule"
-  ]
+module "ingress_deploy" {
+  source  = "fuchicorp/chart/helm"
+  deployment_name        = "ingress"
+  deployment_environment = "${var.deployment_environment}"
+  deployment_endpoint    = "${var.google_domain_name}"
+  deployment_path        = "main-helm"
 
-  wait      = true
-  name      = "services-ingress-${var.deployment_environment}"
-  namespace = "${var.deployment_environment}"
-  chart     = "./main-helm"
-
-  set {
-    name  = "nexusport"
-    value = "${var.nexus_service_port}"
-  }
-
-  set {
-    name  = "vaultport"
-    value = "${var.vault_service_port}"
-  }
-
-  set {
-    name  = "repo_port"
-    value = "${var.repo_port}"
-  }
-
-  set {
-    name  = "email"
-    value = "${var.email}"
-  }
-
-  set {
-    name = "domain_name"
-    value = "${var.google_domain_name}"
+  template_custom_vars = {
+    
+    nexusport = "${var.nexus_service_port}"
+    vaultport = "${var.vault_service_port}"
+    repo_port = "${var.repo_port}"
+    email     = "${var.email}"
+    domain_name = "${var.google_domain_name}"
   }
 }
 
