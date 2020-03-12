f4853ea5bc636bfb488b298475167e6e4e89e241
mo <mkarimidevops@gmail.com>
2020-03-11 19:47:07 -0500

    readonly for service account changed to false

--- google_k8s_dashboard_module.tf
+++ google_k8s_dashboard_module.tf
@@ -1,6 +1,5 @@
 module "dashboard_deployer" {
   source  = "fuchicorp/chart/helm"
-  version = "0.0.2"
   deployment_name        = "dashboard-${var.deployment_environment}"
   deployment_environment = "${var.deployment_environment}"
   deployment_endpoint    = "dashboard.${var.google_domain_name}"
