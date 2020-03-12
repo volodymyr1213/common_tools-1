e746630e3f1f81ed295b069a024862e3532fe3cd
mo <mkarimidevops@gmail.com>
2020-03-11 19:49:09 -0500

    deploy env has changed

--- google_k8s_dashboard_module.tf
+++ google_k8s_dashboard_module.tf
@@ -1,7 +1,7 @@
 module "dashboard_deployer" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "dashboard-${var.deployment_environment}"
-  deployment_environment = "${var.deployment_environment}"
+  deployment_environment = "kube-system"
   deployment_endpoint    = "dashboard.${var.google_domain_name}"
   deployment_path        = "kubernetes-dashboard"
 
