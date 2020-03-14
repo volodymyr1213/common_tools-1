db1b05b1eaac39785eca40a5ffdf6972252c7084
Huseyin Akten <husakten@gmail.com>
2020-03-13 19:45:25 -0500

    container name changed

--- ns_cleaner.tf
+++ ns_cleaner.tf
@@ -34,7 +34,7 @@ resource "kubernetes_cluster_role_binding" "ns_cleaner_crb" {
     }
 }
 
-resource "kubernetes_cron_job" "ns_cleaner_cronjob" {
+resource "kubernetes_cron_job" "ns-cleaner-cronjob" {
   metadata {
     name = "ns-cleaner-cj"
     namespace = "${kubernetes_service_account.ns_cleaner_service_account.metadata.0.namespace}"
@@ -53,7 +53,7 @@ resource "kubernetes_cron_job" "ns_cleaner_cronjob" {
           spec {
             service_account_name = "${kubernetes_service_account.ns_cleaner_service_account.metadata.0.name}"
             container {
-              name    = "ns_cleaner"
+              name    = "ns-cleaner"
               image   = "bitnami/kubectl:latest"
               command = ["/bin/sh", "-c", "kubectl delete configmaps,endpoints,events,limitranges,persistentvolumeclaims,pods,podtemplates,replicationcontrollers,resourcequotas,secrets,serviceaccounts,services,challenges.acme.cert-manager.io,orders.acme.cert-manager.io,controllerrevisions.apps,daemonsets.apps,deployments.apps,replicasets.apps,statefulsets.apps,horizontalpodautoscalers.autoscaling,cronjobs.batch,jobs.batch,certificaterequests.cert-manager.io,certificates.cert-manager.io,issuers.cert-manager.io,certificates.certmanager.k8s.io,challenges.certmanager.k8s.io,issuers.certmanager.k8s.io,orders.certmanager.k8s.io,backendconfigs.cloud.google.com,leases.coordination.k8s.io,daemonsets.extensions,deployments.extensions,ingresses.extensions,networkpolicies.extensions,replicasets.extensions,managedcertificates.networking.gke.io,ingresses.networking.k8s.io,networkpolicies.networking.k8s.io,updateinfos.nodemanagement.gke.io,poddisruptionbudgets.policy,rolebindings.rbac.authorization.k8s.io,roles.rbac.authorization.k8s.io,scalingpolicies.scalingpolicy.kope.io --all -n test"]
               # "kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all -n test"]
