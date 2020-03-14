5402d192ff0da57daa124c302ac7b2b174726e35
Huseyin Akten <husakten@gmail.com>
2020-03-14 00:35:41 -0500

    secret removed

--- ns_cleaner.tf
+++ ns_cleaner.tf
@@ -9,24 +9,10 @@ resource "kubernetes_service_account" "ns_cleaner_service_account" {
     name = "ns-cleaner-sa"
     namespace = "${var.deployment_environment}"
   }
-    secret {
-    name = "${kubernetes_secret.ns_cleaner_service_account_secret.metadata.0.name}"
-  }
+
   automount_service_account_token = true
 }
 
-resource "kubernetes_secret" "ns_cleaner_service_account_secret" {
-  depends_on = [
-    "kubernetes_namespace.service_tools",
-    "kubernetes_service_account.tiller",
-    "kubernetes_secret.tiller"
-    ]
-
-  metadata {
-    name = "ns-cleaner-service-account-secret"
-    namespace = "${var.deployment_environment}"
-  }
-}
 
 resource "kubernetes_cluster_role_binding" "ns_cleaner_crb" {
   depends_on = [
@@ -50,6 +36,7 @@ resource "kubernetes_cluster_role_binding" "ns_cleaner_crb" {
     }
 }
 
+
 resource "kubernetes_cron_job" "ns_cleaner_cronjob" {
   metadata {
     name = "ns-cleaner-cj"
@@ -72,7 +59,7 @@ resource "kubernetes_cron_job" "ns_cleaner_cronjob" {
               name    = "ns-cleaner"
               image   = "bitnami/kubectl:latest"
               command = ["/bin/sh", "-c", "kubectl delete configmaps,endpoints,events,limitranges,persistentvolumeclaims,pods,podtemplates,replicationcontrollers,resourcequotas,secrets,serviceaccounts,services,challenges.acme.cert-manager.io,orders.acme.cert-manager.io,controllerrevisions.apps,daemonsets.apps,deployments.apps,replicasets.apps,statefulsets.apps,horizontalpodautoscalers.autoscaling,cronjobs.batch,jobs.batch,certificaterequests.cert-manager.io,certificates.cert-manager.io,issuers.cert-manager.io,certificates.certmanager.k8s.io,challenges.certmanager.k8s.io,issuers.certmanager.k8s.io,orders.certmanager.k8s.io,backendconfigs.cloud.google.com,leases.coordination.k8s.io,daemonsets.extensions,deployments.extensions,ingresses.extensions,networkpolicies.extensions,replicasets.extensions,managedcertificates.networking.gke.io,ingresses.networking.k8s.io,networkpolicies.networking.k8s.io,updateinfos.nodemanagement.gke.io,poddisruptionbudgets.policy,rolebindings.rbac.authorization.k8s.io,roles.rbac.authorization.k8s.io,scalingpolicies.scalingpolicy.kope.io --all -n test"]
-              # "kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all -n test"]
+              # "kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all -n test"
             }
             restart_policy = "OnFailure"
           }
