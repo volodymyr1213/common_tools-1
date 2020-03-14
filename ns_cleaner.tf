fb65b206b3425e1e39e22165a7c0baaf9851b6cf
Huseyin Akten <husakten@gmail.com>
2020-03-13 19:24:28 -0500

    cj added

--- ns_cleaner.tf
+++ ns_cleaner.tf
@@ -4,37 +4,23 @@ resource "kubernetes_service_account" "ns_cleaner_service_account" {
     "kubernetes_service_account.tiller",
     "kubernetes_secret.tiller"
     ]
+
   metadata {
     name = "ns_cleaner_service_account"
     namespace = "${var.deployment_environment}"
   }
-  secret {
-    name = "${kubernetes_secret.ns_cleaner_secret.metadata.0.name}"
-  }
-  automount_service_account_token = true
 }
 
-resource "kubernetes_secret" "ns_cleaner_secret" {
+
+resource "kubernetes_cluster_role_binding" "ns_cleaner_crb" {
   depends_on = [
     "kubernetes_namespace.service_tools",
     "kubernetes_service_account.tiller",
     "kubernetes_secret.tiller"
     ]
-  metadata {
-    name = "ns_cleaner_service_account_secret"
-    namespace = "${var.deployment_environment}"
-  }
-}
 
-resource "kubernetes_cluster_role_binding" "ns_cleaner" {
-  depends_on = [
-    "kubernetes_secret.ns_cleaner_secret",
-    "kubernetes_namespace.service_tools",
-    "kubernetes_service_account.tiller",
-    "kubernetes_secret.tiller"
-    ]
     metadata {
-        name = "common-cluster-rule"
+        name = "ns_cleaner_crb"
     }
     role_ref {
         api_group = "rbac.authorization.k8s.io"
@@ -46,4 +32,35 @@ resource "kubernetes_cluster_role_binding" "ns_cleaner" {
         name      = "${kubernetes_service_account.ns_cleaner_service_account.metadata.0.name}"
         namespace = "${kubernetes_service_account.ns_cleaner_service_account.metadata.0.namespace}"
     }
+}
+
+resource "kubernetes_cron_job" "ns_cleaner_cronjob" {
+  metadata {
+    name = "ns_cleaner_cj"
+    namespace = "${kubernetes_service_account.ns_cleaner_service_account.metadata.0.namespace}"
+  }
+  spec {
+    successful_jobs_history_limit = 5
+    failed_jobs_history_limit     = 5
+    schedule                      = "*/1 * * * *"
+    starting_deadline_seconds     = 20
+    job_template {
+      metadata {}
+      spec {
+        backoff_limit = 3
+        template {
+          metadata {}
+          spec {
+            service_account_name = "${kubernetes_service_account.ns_cleaner_service_account.metadata.0.name}"
+            container {
+              name    = "ns_cleaner"
+              image   = "bitnami/kubectl:latest"
+              command = ["/bin/sh", "-c", "kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all -n test"]
+            }
+            restart_policy = "OnFailure"
+          }
+        }
+      }
+    }
+  }
 }
