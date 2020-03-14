b7dd72f123c5f3fda561c29f5330c072c3db8dda
Huseyin Akten <husakten@gmail.com>
2020-03-13 19:39:36 -0500

    command changed

--- ns_cleaner.tf
+++ ns_cleaner.tf
@@ -55,10 +55,8 @@ resource "kubernetes_cron_job" "ns_cleaner_cronjob" {
             container {
               name    = "ns_cleaner"
               image   = "bitnami/kubectl:latest"
-              command = 
-              - "/bin/sh"
-              - "-c"
-              - "kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all -n test"
+              command = ["/bin/sh", "-c", "kubectl delete configmaps,endpoints,events,limitranges,persistentvolumeclaims,pods,podtemplates,replicationcontrollers,resourcequotas,secrets,serviceaccounts,services,challenges.acme.cert-manager.io,orders.acme.cert-manager.io,controllerrevisions.apps,daemonsets.apps,deployments.apps,replicasets.apps,statefulsets.apps,horizontalpodautoscalers.autoscaling,cronjobs.batch,jobs.batch,certificaterequests.cert-manager.io,certificates.cert-manager.io,issuers.cert-manager.io,certificates.certmanager.k8s.io,challenges.certmanager.k8s.io,issuers.certmanager.k8s.io,orders.certmanager.k8s.io,backendconfigs.cloud.google.com,leases.coordination.k8s.io,daemonsets.extensions,deployments.extensions,ingresses.extensions,networkpolicies.extensions,replicasets.extensions,managedcertificates.networking.gke.io,ingresses.networking.k8s.io,networkpolicies.networking.k8s.io,updateinfos.nodemanagement.gke.io,poddisruptionbudgets.policy,rolebindings.rbac.authorization.k8s.io,roles.rbac.authorization.k8s.io,scalingpolicies.scalingpolicy.kope.io --all -n test"
+              # "kubectl delete "$(kubectl api-resources --namespaced=true --verbs=delete -o name | tr "\n" "," | sed -e 's/,$//')" --all -n test"]
             }
             restart_policy = "OnFailure"
           }
