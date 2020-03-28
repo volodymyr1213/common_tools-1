fd5aa7f7ea4462da131bafa28e92cb71094a55f4
Huseyin Akten <husakten@gmail.com>
2020-03-27 19:33:06 -0500

    dependencies are configured for apply and destroy

--- google_k8s_ingress_controller_module.tf
+++ google_k8s_ingress_controller_module.tf
@@ -1,11 +1,13 @@
 module "ingress_controller" {
   source  = "fuchicorp/chart/helm"
   deployment_name        = "ingress-controller-${var.deployment_environment}"
-  deployment_environment = "${var.deployment_environment}"
+  deployment_environment = "${kubernetes_namespace.service_tools.metadata.0.name}"
   deployment_endpoint    = "ingress-controller.${var.google_domain_name}"
   deployment_path        = "./ingress-controller"
 
-
+  template_custom_vars   = {
+    null_depends_on      = "${null_resource.cert_manager.id}"
+  }
 }
 
 
--- google_k8s_service_jenkins_official_module.tf
+++ google_k8s_service_jenkins_official_module.tf
@@ -2,12 +2,14 @@ module "jenkins_deploy" {
   source = "fuchicorp/chart/helm"
 
   deployment_name        = "jenkins-deployment"
-  deployment_environment = "${var.deployment_environment}"
+  deployment_environment = "${kubernetes_namespace.service_tools.metadata.0.name}"
   deployment_endpoint    = "jenkins.${var.google_domain_name}"
   deployment_path        = "jenkins"
 
   template_custom_vars = {
-     jenkins_user           = "${var.jenkins["admin_user"]}"
+    
+    null_depends_on        = "${null_resource.cert_manager.id}"
+    jenkins_user           = "${var.jenkins["admin_user"]}"
     jenkins_pass           = "${var.jenkins["admin_password"]}"
     jenkins_auth_secret    = "${var.jenkins["jenkins_auth_secret"]}"
     jenkins_auth_client_id = "${var.jenkins["jenkins_auth_client_id"]}"
