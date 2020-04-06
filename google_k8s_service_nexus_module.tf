f9c4e181100f72f603eb4e4c5242d393a8bc551c
Murodbey <abdullayevmurod@gmail.com>
2020-04-06 14:15:58 -0500

    final change

--- google_k8s_service_nexus_module.tf
+++ google_k8s_service_nexus_module.tf
@@ -1,13 +1,13 @@
 module "nexus_deploy" {
-  source  = "fuchicorp/chart/helm"
+  source                 = "fuchicorp/chart/helm"
   deployment_name        = "nexus"
   deployment_environment = "${kubernetes_namespace.service_tools.metadata.0.name}"
   deployment_endpoint    = "nexus.${var.google_domain_name}"
-  deployment_path        = "sonatype-nexus"
-
+  deployment_path        = "nexus"
   template_custom_vars = {
-    null_depends_on          = "${null_resource.helm_init.id}"
-    docker_endpoint          = "docker.${var.google_domain_name}"
-    repo_port                = "${var.repo_port}"
+    null_depends_on = "${null_resource.cert_manager.id}"
+    docker_endpoint = "docker.${var.google_domain_name}"
+    nexus_port      = "${var.nexus_service_port}"
+    repo_port       = "${var.repo_port}"
   }
 }
