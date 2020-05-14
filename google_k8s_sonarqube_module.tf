4c992a8271ad70ecdf3d35d15c4854c8c759f0e4
Murod Abdullaev <Murodbey@bastion-fuchicorp.us-central1-a.c.gold-order-274923.internal>
2020-05-14 23:10:17 +0000

    sonarqube chart created

---
+++ google_k8s_sonarqube_module.tf
@@ -0,0 +1,12 @@
+module "sonarqube_deploy" {
+  source                 = "fuchicorp/chart/helm"
+  deployment_name        = "sonarqube"
+  deployment_environment = "${kubernetes_namespace.service_tools.metadata.0.name}"
+  deployment_endpoint    = "sonarqube.${var.google_domain_name}"
+  deployment_path        = "sonarqube"
+
+  template_custom_vars = {
+
+    null_depends_on      = "${null_resource.cert_manager.id}"
+  }
+}
